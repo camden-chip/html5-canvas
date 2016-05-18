3cf6f7417227e3de93d85c8fb6f979394fd90668
Tatsiana Chypsanava <camden_chip@hotmail.com>
2016-05-19 10:58:15 +1200

    added caman photo editor

---
+++ js/use-caman.js
@@ -0,0 +1,20 @@
+var photoCanvas = document.querySelector('#photo');
+var photoCtx = photoCanvas.getContext('2d');
+
+// Get an image from Flickr
+var image = new Image();
+
+image.src = 'http://loremflickr.com/200/200';
+
+image.onload = function() {
+
+	// Image is ready to be manipulated / used
+	photoCtx.drawImage( image, 0, 0 );
+
+}
+
+Caman('#filters' , 'images/200.jpeg', function() {
+	
+	this.sepia();
+	this.render();
+});
